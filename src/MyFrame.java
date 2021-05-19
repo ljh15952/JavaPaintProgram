ae2eefb28cf29452238025a67b7fdf4ffa2b899b
Can I? <ljh15952@gmail.com>
2021-05-19 20:17:48 +0900

    Pop up JFrame

---
+++ src/MyFrame.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+public class MyFrame extends JFrame{
+	public MyFrame() {
+		setSize(500,500);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+
+	public void Init() {
+		
+	}
+}
